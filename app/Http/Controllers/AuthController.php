a51c2bf524c5db86bad10329a019ba1f35897ac5
Muhammad Talha Khan <talha208@ucp.edu.pk>
2019-06-26 18:16:30 +0500

    created auth controller for login endpoint class

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers;
+use Validator;
+use App\User;
+use Firebase\JWT\JWT;
+use Illuminate\Http\Request;
+use Firebase\JWT\ExpiredException;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Lumen\Routing\Controller as BaseController;
+class AuthController extends BaseController 
+{
+	
+}
