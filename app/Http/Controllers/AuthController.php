5e9e322b47910b755a83fc1f58dd033698360d3f
Muhammad Talha Khan <talha208@ucp.edu.pk>
2019-06-26 18:27:53 +0500

    added authenticate method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -40,7 +40,39 @@ class AuthController extends BaseController
         // As you can see we are passing `JWT_SECRET` as the second parameter that will 
         // be used to decode the token in the future.
         return JWT::encode($payload, env('JWT_SECRET'));
-    } 
+    }
+        /**
+     * Authenticate a user and return the token if the provided credentials are correct.
+     * 
+     * @param  \App\User   $user 
+     * @return mixed
+     */
+    public function authenticate(User $user) {
+        $this->validate($this->request, [
+            'email'     => 'required|email',
+            'password'  => 'required'
+        ]);
+        // Find the user by email
+        $user = User::where('email', $this->request->input('email'))->first();
+        if (!$user) {
+            // You wil probably have some sort of helpers or whatever
+            // to make sure that you have the same response format for
+            // differents kind of responses. But let's return the 
+            // below respose for now.
+            return response()->json([
+                'error' => 'Email does not exist.'
+            ], 400);
+        }
+        // Verify the password and generate the token
+        if (Hash::check($this->request->input('password'), $user->password)) {
+            return response()->json([
+                'token' => $this->jwt($user)
+            ], 200);
+        }
+ 
+
+
+
 
 
 
