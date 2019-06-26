19e4d78e8907a27ed1438aa175726c382ba1c8b9
Muhammad Talha Khan <talha208@ucp.edu.pk>
2019-06-26 18:20:17 +0500

    added method to generate jwt token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,5 +29,19 @@ class AuthController extends BaseController
      * 
      * @param  \App\User   $user
      * @return string
-     */	
+     */
+    protected function jwt(User $user) {
+        $payload = [
+            'iss' => "lumen-jwt", // Issuer of the token
+            'sub' => $user->id, // Subject of the token
+            'iat' => time(), // Time when JWT was issued. 
+            'exp' => time() + 60*60 // Expiration time
+        ];
+        // As you can see we are passing `JWT_SECRET` as the second parameter that will 
+        // be used to decode the token in the future.
+        return JWT::encode($payload, env('JWT_SECRET'));
+    } 
+
+
+
 }
